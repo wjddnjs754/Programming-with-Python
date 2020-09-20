31102277f79ad4438c78759a1bb1077aa5aebd5b
wjddnjs754 <wjddnjs754@naver.com>
2020-09-20 23:23:58 +0900

    Create #10869 사칙연산(input, split).py
    BOJ with Python
    #10869 사칙연산(input, split사용)

---
+++ BOJ/#10869 사칙연산(input, split).py
@@ -0,0 +1,13 @@
+#Date:2020/09/20
+#BOJ 10869 "사칙연산"
+
+M = input().split()
+A=int(M[0])
+B=int(M[1])
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
+#예제 입력: 1 2
+#입력이 띄어쓰기 되어있기 때문에 split 사용
